02066b68362a181607679e1905ae991a95be8cbb
MunyinyaMaurice <munyinya13@gmail.com>
2026-03-27 00:29:55 +0200

    save work before pulling main

--- src/main/java/com/backend/rentalBusiness/common/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.backend.rentalBusiness.common.exception;
-
-public class ResourceNotFoundException extends ApiException {
-
-    public ResourceNotFoundException(String resource) {
-        super(resource + " not found");
-    }
-}
